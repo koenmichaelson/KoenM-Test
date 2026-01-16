b53ec77f76f5e1d763bbaf3c45dca9d68496aa11
SillyGoose210 <koenmichaelson@gmail.com>
2026-01-15 18:36:11 -0700

    Random Guessing Game

---
+++ out/build/x64-Debug/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: C:/Users/koenm/source/repos/KoenM-Test
+# Build directory: C:/Users/koenm/source/repos/KoenM-Test/out/build/x64-Debug
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test(TestExample_test "TestExample" "1")
+set_tests_properties(TestExample_test PROPERTIES  _BACKTRACE_TRIPLES "C:/Users/koenm/source/repos/KoenM-Test/CMakeLists.txt;21;add_test;C:/Users/koenm/source/repos/KoenM-Test/CMakeLists.txt;0;")
--- src/TestExample.cpp
+++ src/TestExample.cpp
@@ -1,1 +1,54 @@
 //TODO: Display "Hello Class!" to the console
+
+#include <iostream>
+#include <string>
+#include <random>
+
+int main() {
+
+	bool PlayAgain;
+
+	int PlayerGuess = 0;
+
+	// Initialize Random Number Generation
+	std::mt19937 ri(static_cast<unsigned int>(std::time(0)));
+	std::uniform_int_distribution<int> randomint(1, 100);
+
+	int RandomNum = randomint(ri);
+	int NumberOfAttempts = 0;
+
+
+	std::cout << "Welcome to The Number Guessing Game but in C++!\n";
+	std::cout << "Guess Your Number\n";
+
+	while (RandomNum != PlayerGuess) {
+
+		std::cin >> PlayerGuess;
+
+		if (PlayerGuess < RandomNum) {
+
+			std::cout << "Your Guess was too low\n";
+			NumberOfAttempts++;
+		}
+		else if (PlayerGuess > RandomNum) {
+
+			std::cout << "Your Guess was too high\n";
+			NumberOfAttempts++;
+		}
+		else {
+
+			std::cout << "Your Guess was Correct!\n";
+			std::cout << "You guessed " << NumberOfAttempts << " amount of times\n";
+			std::cout << "Thanks for Playing!";
+
+		}
+
+	}
+
+
+	
+
+
+
+	return 0;
+}
